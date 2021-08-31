6a9bf2f9d0b79841313515a2ad5a4a76faf76088
Anuja <anujanikumbh5@GMAIL.COM>
2021-09-01 00:14:31 +0530

    models created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+#models created
+class Book(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    author = models.CharField(max_length=100, blank=True)
+    isbn = models.CharField(max_length=100, blank=True)
+    publisher = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.title

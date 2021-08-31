ae203c27d10e043bc6b09628c8bf0c6162c53308
Anuja <anujanikumbh5@GMAIL.COM>
2021-09-01 00:15:30 +0530

    views created

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from api.models import Book
+from api.serializers import BookSerializer
 # Create your views here.
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView): #toupdate and del records
+    queryset = Book
+    serializer_class = BookSerializer
+
+

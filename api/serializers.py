fb0127f385d100c48c436e8d4658f84a512d9082
Anuja <anujanikumbh5@GMAIL.COM>
2021-09-01 20:57:36 +0530

    changed serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from api.models import Book
+from .models import Book
 
 #serializerscreated
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id','title','author','isbn','publisher']
+        fields = '__all__'

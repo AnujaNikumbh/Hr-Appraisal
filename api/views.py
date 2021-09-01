12aa9d2ba1b8dcc5f211fc58c4cbf61109b67b76
Anuja <anujanikumbh5@GMAIL.COM>
2021-09-01 20:51:44 +0530

    views created

--- api/views.py
+++ api/views.py
@@ -1,15 +1,63 @@
+from django.http import response
 from django.shortcuts import render
-from rest_framework import generics
-from api.models import Book
-from api.serializers import BookSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+#from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 # Create your views here.
 
-class BookList(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
 
-class BookDetail(generics.RetrieveUpdateDestroyAPIView): #toupdate and del records
-    queryset = Book
-    serializer_class = BookSerializer
+'''@api_view(['GET'])
+def apiOverview(request):
+    api_urls={
+        'List':'/book-list/',
+        'Detail view': '/book-detail/<int:id>/', 
+        'Create':'/product-create/',
+        'Update': '/book-update/<int:id>/', 
+        'Delete': '/book-detail/<int:id>/',
 
 
+    }   
+
+    return Response(api_urls)'''
+    
+@api_view(['GET'])
+def ShowAll(request):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def ViewBook(request,pk):
+    book = Book.objects.get(id=pk)
+    serializer = BookSerializer(book, many=False)
+    return Response(serializer.data)
+
+    
+@api_view(['POST'])
+def CreateBook(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def updateBook(request,pk):
+    book = Book.objects.get(id=pk)
+    serializer = BookSerializer(instance=book, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)   
+
+
+@api_view(['GET'])
+def deleteBook(request,pk):
+    book = Book.objects.get(id=pk)
+    book.delete()
+    return Response('Items deleted successfully')      
+
+
+   

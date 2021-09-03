dabc30061aec8b7127593726b2bf3af7af43b93b
Anuja <anujanikumbh5@GMAIL.COM>
2021-09-03 18:38:54 +0530

    updated variable names in proper format

--- api/models.py
+++ api/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 #models created
 class Book(models.Model):
-   book_title = models.CharField(max_length=200, null=False, blank=False)
-   book_category = models.CharField(max_length=100,null=False, blank=False)
-   book_price = models.CharField(max_length=200, null=False, blank=False)#models.DecimalField(max_digits=4,decimal_places=2)
-   book_description = models.TextField()
-   stars = models.IntegerField()
+   book_title = models.CharField(max_length=200, null=False, blank=False) #changed variable name
+   book_category = models.CharField(max_length=100,null=False, blank=False) #changed variable name
+   book_price = models.CharField(max_length=200, null=False, blank=False)#models.DecimalField(max_digits=4,decimal_places=2)#changed variable name
+   book_description = models.TextField()#changed variable name
+   stars = models.IntegerField() #changed variable name
 
 
    def __str__(self):

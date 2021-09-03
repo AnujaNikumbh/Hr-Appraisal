0ae26022e7aa4a75f1907d36cb3f25b8acddcabd
Anuja <anujanikumbh5@GMAIL.COM>
2021-09-03 18:39:28 +0530

    urls added

--- api/urls.py
+++ api/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     #path('',views.apiOverview, name='apiOverview'),
     path('book-list/', views.showAll, name='book-list'),  #changed function initial to lower case
     path('book-detail/<int:pk>/', views.viewBook, name='book-detail'),#changed function initial to lower case
-    path('book-create/', views.createBook, name='book-create'),#changed function initial to lower casegit
+    path('book-create/', views.createBook, name='book-create'),#changed function initial to lower case
     path('book-update/<int:pk>/', views.updateBook, name='book-update'),
     path('book-delete/<int:pk>/', views.deleteBook, name='book-delete'),
     
